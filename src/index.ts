87bf848230d1adf167cfad5bb87d635483aeab6d
Hugo Violas <violas.hugo@gmail.com>
2023-04-29 12:08:03 +0200

    Work with streams

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import { Readable, Writable } from "node:stream";
+import { createReadStream } from "fs";
+import * as path from "path";
+
+const relativePath = "./public/BigData.json";
+const fullPath = path.join(process.cwd(), relativePath);
+const read: Readable = createReadStream(fullPath);
+
+read.on("data", (chunk) => {
+  console.log("CHUNK", chunk.toString());
+});
